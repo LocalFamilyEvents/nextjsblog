f58546b34c0af29d1c23785fa56f61aae52c60c4
Simon Hurley <simon.hurley@elateral.com>
2022-01-10 00:23:32 +0000

    Add dates component

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,18 +1,23 @@
 import { remark } from 'remark'
 import html from 'remark-html'
+import Head from 'next/head'
 
 import { getAllPostIds, getPostData } from '../../lib/posts'
 
+import Date from '../../components/dates'
 import Layout from '../../components/layout'
 
 export default function Post({ postData }) {
     return (
         <Layout>
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
             {postData.title}
             <br />
             {postData.id}
             <br />
-            {postData.date}
+            <Date dateString={postData.date} />
             <br/>
             <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
         </Layout>
