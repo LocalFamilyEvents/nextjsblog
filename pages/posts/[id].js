57a1e7a9d9a1690f7592fc67b95f5959ba5e080f
Simon Hurley <simon.hurley@elateral.com>
2022-01-10 00:25:51 +0000

    styled blog pages using utilstyles

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,41 +1,44 @@
-import { remark } from 'remark'
-import html from 'remark-html'
-import Head from 'next/head'
+import { remark } from "remark";
+import html from "remark-html";
+import Head from "next/head";
 
-import { getAllPostIds, getPostData } from '../../lib/posts'
+import { getAllPostIds, getPostData } from "../../lib/posts";
 
-import Date from '../../components/dates'
-import Layout from '../../components/layout'
+import Date from "../../components/dates";
+import Layout from "../../components/layout";
+
+import utilStyles from "../../styles/utils.module.css";
 
 export default function Post({ postData }) {
-    return (
-        <Layout>
-            <Head>
-                <title>{postData.title}</title>
-            </Head>
-            {postData.title}
-            <br />
-            {postData.id}
-            <br />
-            <Date dateString={postData.date} />
-            <br/>
-            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-        </Layout>
-    )
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
 }
 
 export async function getStaticPaths() {
-    const paths = getAllPostIds();
-    return {
-        paths, fallback: false
-    }
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
 }
 
 export async function getStaticProps({ params }) {
-    const postData = await getPostData(params.id)
-    return {
-        props: {
-            postData
-        }
-    }
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
 }
