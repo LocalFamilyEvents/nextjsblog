f8cec6a129b4df648662846feb32c356bd8c70b7
Simon Hurley <simon.hurley@elateral.com>
2022-01-11 00:34:36 +0000

    added custom 404

---
+++ pages/404.js
@@ -0,0 +1,5 @@
+import Layout from "../components/layout"
+
+export default function Custom404() {
+    return <Layout><h1>Opps - You appear to be looking for something that doesn't or no longer exists</h1></Layout>
+}
